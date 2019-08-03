49117b4120ccda8311736f94b7348abc2298a984
Alex Clark <aclark@aclark.net>
2019-08-03 17:28:39 -0400

    Update

--- application.py
+++ application.py
@@ -2,6 +2,6 @@
 #     start_fn('200 OK', [('Content-Type', 'text/html')])
 #     return ["<h1>Hello world!</h1>\n"]
 
-from Zope2.startup.run import make_wsgi_app
+from Zope2.Startup.run import make_wsgi_app
 
 application = make_wsgi_app({}, '/srv/parts/plone/etc/zope.conf')
